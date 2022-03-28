7c2cec08769c7a9ff76977ebf679075b4ce9f32b
vinamrahw <76243844+vinamrahw@users.noreply.github.com>
2022-03-28 19:07:38 +0530

    Update backend.tf
    modified s3 bucket and region

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "week-24-project-07162021"
+    bucket = "vinam-backend"
     key    = "remote.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
